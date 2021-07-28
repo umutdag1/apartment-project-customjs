4d3c13257a2dcef2e84cfe6e983f87b18ec337ec
Umut Dag <umutdag156@gmail.com>
2021-07-28 04:13:09 +0300

    Last Changes Of Day

--- src/App.vue
+++ src/App.vue
@@ -6,32 +6,10 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {
-    const scriptArr = [
-      "assets/plugins/jquery/jquery.min.js",
-      "assets/plugins/bootstrap/js/bootstrap.bundle.min.js",
-      "assets/dist/js/adminlte.min.js",
-      "assets/custom/js/script.js",
-    ];
-
-    let scriptIndex = 0;
-
-    function addNextScriptToBody() {
-      const scriptElem = document.createElement("script");
-      scriptElem.setAttribute("src", scriptArr[scriptIndex++]);
-      document.body.appendChild(scriptElem);
-      scriptElem.addEventListener(
-        "load",
-        function () {
-          if (scriptArr.length !== scriptIndex) {
-            addNextScriptToBody();
-          }
-        },
-        false
-      );
-    }
-
-    addNextScriptToBody();
+  data() {
+    return {
+      loader: null,
+    };
   },
   methods: {
     makePostRequest(configParams) {
@@ -122,15 +100,43 @@ export default defineComponent({
       });
     },
   },
-  mounted() {
-    const loader = this.$loading.show({
+  created() {
+    const scriptArr = [
+      "assets/plugins/jquery/jquery.min.js",
+      "assets/plugins/bootstrap/js/bootstrap.bundle.min.js",
+      "assets/dist/js/adminlte.min.js",
+      "assets/custom/js/script.js",
+    ];
+
+    let scriptIndex = 0;
+
+    function addNextScriptToBody() {
+      const scriptElem = document.createElement("script");
+      scriptElem.setAttribute("src", scriptArr[scriptIndex++]);
+      document.body.appendChild(scriptElem);
+      scriptElem.addEventListener(
+        "load",
+        function () {
+          if (scriptArr.length !== scriptIndex) {
+            addNextScriptToBody();
+          }
+        },
+        false
+      );
+    }
+
+    addNextScriptToBody();
+  },
+  beforeMount() {
+    this.loader = this.$loading.show({
       container: null,
       opacity: 1,
       canCancel: false,
     });
-
+  },
+  mounted() {
     setTimeout(() => {
-      loader.hide();
+      this.loader.hide();
     }, 1000);
 
     this.emitter.on("fireToast", (data) => {
